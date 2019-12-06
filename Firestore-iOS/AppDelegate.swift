4f40117a22c75f356aec64435354ba3a2e8538cf
enoreyes <enoreyes@gmail.com>
2019-12-06 01:37:27 -0500

    - Updated Authentication

--- Firestore-iOS/AppDelegate.swift
+++ Firestore-iOS/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Firebase
-import SendBirdSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        SBDMain.initWithApplicationId(Constants.sendBirdID)
         // Override point for customization after application launch.
         return true
     }
