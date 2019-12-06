b30fc905f73333853b5f226adb930c307cc75b44
Max <mnfu@princeton.edu>
2019-12-06 01:01:39 -0500

    Profile image, bio, year integration with validation of fields

--- Firestore-iOS/ViewControllers/OnboardingViewController.swift
+++ Firestore-iOS/ViewControllers/OnboardingViewController.swift
@@ -12,36 +12,21 @@ import Firebase
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var classText: UITextField!
+    @IBOutlet weak var bioText: UITextField!
+    @IBOutlet weak var swipeButton: UIButton!
     @IBOutlet weak var userPhoto: UIImageView!
-    
+    var defaultImage = "defaultImage"
     var storageRef = Storage.storage().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        swipeButton.layer.cornerRadius = 10
+        userPhoto.image = UIImage(named: defaultImage)
+        hideErrorLabel()
         // Do any additional setup after loading the view.
     }
-    
-//    func uploadImagePic(img1 :UIImage){
-//        var data = NSData()
-//        data = img1.jpegData(compressionQuality: 0.8)! as NSData
-//        // set upload path
-//        let filePath = "images/1324.jpg" // path where you wanted to store img in storage
-//        let metaData = StorageMetadata()
-//        metaData.contentType = "image/jpg"
-//
-//        self.storageRef = Storage.storage().reference()
-//        self.storageRef.child(filePath).putData(data as Data, metadata: metaData){(metaData,error) in
-//            if let error = error {
-//                print(error.localizedDescription)
-//                return
-//            }else{
-//                //store downloadURL
-//                let downloadURL = metaData?.path
-//
-//            }
-//        }
-//    }
     
     @IBAction func uploadImageTapped(_  sender: Any) {
         let picker = UIImagePickerController()
@@ -50,13 +35,31 @@ class OnboardingViewController: UIViewController {
         present(picker, animated:true, completion: nil)
 
     }
+    @IBAction func startSwipingTapped(_  sender: Any){
+        if let text = bioText.text, !text.isEmpty && ((classText?.text) != nil), !text.isEmpty {
+            let db = Firestore.firestore()
+            let uid = Auth.auth().currentUser!.uid
+            db.collection("users").document(uid).updateData(["description": bioText.text as Any, "classYear": classText.text as Any], completion: { (error) in
+                // something went wrong when saving first and last name
+                if error != nil {
+                    self.showError("User data couldn't be saved properly")
+                }
+            })
+            self.transitionToHome()
+        }
+        else{
+            self.showError("please fill in above fields!")
+        }
+        
+    }
 
 }
 
 extension OnboardingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
+        // random string as placeholder for image filename
+        let randomID = UUID.init().uuidString
         let originalImage = info[.originalImage] as? UIImage
         userPhoto.image = originalImage
         
@@ -64,27 +67,55 @@ extension OnboardingViewController: UIImagePickerControllerDelegate, UINavigatio
         var data = NSData()
         data = userPhoto.image!.jpegData(compressionQuality: 0.8)! as NSData
         // set upload path
-        let filePath = "images/hello.jpg"
+        let filePath = "images/\(randomID).jpg"
       
         //let filePath = "\(Auth.auth().currentUser!.uid)/\("userPhoto")"
-        let metaData = StorageMetadata()
+        let metaData = StorageMetadata.init()
         metaData.contentType = "image/jpg"
-        self.storageRef.child(filePath).putData(data as Data, metadata: metaData){(metaData,error) in
+        self.storageRef.child(filePath).putData(data as Data, metadata: metaData){(downloadmetaData,error) in
             if let error = error {
-                print(error.localizedDescription)
+                self.showError(error.localizedDescription)
                 return
             }else{
-            //store downloadURL
-                //let downloadURL = (metaData!.downloadURL() as AnyObject).absoluteString
-            //store downloadURL at database
-               print("yay") //self.databaseRef.child("users").child(Auth.auth()!.currentUser!.uid).updateChildValues(["userPhoto": downloadURL])
+            //store downloadURL in database
+                let uploadRef = Storage.storage().reference(withPath: filePath)
+                uploadRef.downloadURL{ url, error in
+                    if let error = error {
+                        self.showError("\(error.localizedDescription)")
+                    }
+                    // updating profileImageURL for user
+                    if let url = url{
+                        let db = Firestore.firestore()
+                        let uid = Auth.auth().currentUser!.uid
+                        db.collection("users").document(uid).updateData(["profileImageUrl": url.absoluteString], completion: { (error) in
+                            // something went wrong when saving first and last name
+                            if error != nil {
+                                self.showError("User data couldn't be saved properly")
+                            }
+                        })
+                    }
+                }
             }
-
-            }
+        }
+    }
+//
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        print("cancelled")
+//    }
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
     }
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print("cancelled")
+    func hideErrorLabel() {
+        errorLabel.alpha = 0
+    }
+    
+    func showError(_ message: String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
     }
     
 }
